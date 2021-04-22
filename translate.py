df3d1822aaa345872eddb8b7c731130816487e76
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2021-04-22 16:15:48 +0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -8,15 +8,14 @@ import random
 start = True
 app = Flask(__name__)
 
+sessionStorage = {}
+
 logging.basicConfig(level=logging.INFO)
+
 
 # создаем словарь, в котором ключ — название города,
 # а значение — массив, где перечислены id картинок,
 # которые мы записали в прошлом пункте.
-
-start = True
-
-
 @app.route('/post', methods=['POST'])
 def main():
     logging.info(f'Request: {request.json!r}')
@@ -33,11 +32,33 @@ def main():
 
 
 def handle_dialog(res, req):
-    global start
-    if start:
+    user_id = req['session']['user_id']
+
+    # если пользователь новый, то просим его представиться.
+    if req['session']['new']:
+        res['response']['text'] = 'Привет! Назови свое имя!'
+        # создаем словарь в который в будущем положим имя пользователя
+        sessionStorage[user_id] = {
+            'first_name': None
+        }
+        return
+
+    # если пользователь не новый, то попадаем сюда.
+    # если поле имени пустое, то это говорит о том,
+    # что пользователь еще не представился.
+    if sessionStorage[user_id]['first_name'] is None:
+        # в последнем его сообщение ищем имя.
+        first_name = get_first_name(req)
+        # если не нашли, то сообщаем пользователю что не расслышали.
+        if first_name is None:
+            res['response']['text'] = \
+                'Не расслышала имя. Повтори, пожалуйста!'
+        # если нашли, то приветствуем пользователя.
+        # И спрашиваем какой город он хочет увидеть.
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
         res['response']['text'] = 'Привет, я переводчик\n' \
                                   'Испльзуйте конструкцию:\n "Переведи слово: (слово для перевода)"'
-        start = False
     else:
         word = req['request']["original_utterance"].split(':')[1]
 
@@ -54,6 +75,17 @@ def handle_dialog(res, req):
 
         res['response']['text'] = response['responseData']['translatedText']
         return
+
+
+def get_first_name(req):
+    # перебираем сущности
+    for entity in req['request']['nlu']['entities']:
+        # находим сущность с типом 'YANDEX.FIO'
+        if entity['type'] == 'YANDEX.FIO':
+            # Если есть сущность с ключом 'first_name',
+            # то возвращаем ее значение.
+            # Во всех остальных случаях возвращаем None.
+            return entity['value'].get('first_name', None)
 
 
 if __name__ == '__main__':
